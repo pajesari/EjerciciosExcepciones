6dee2e128f95f273bb7aaa98a25b3d8aff2b48de
pablo salazar riou <ryuuuu456@gmail.com>
2026-05-12 16:38:23 +0000

    entrada desde archivo de texto

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,13 +1,18 @@
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;
 
 public class Ejercicio1{
     public static void main(String [] args){
-        Scanner teclado= new Scanner(System.in);
+        File arch = new File("entrada.txt");
+        Scanner teclado = null;
         int a, b;
         
         try{
+            teclado= new Scanner(arch);
             System.out.println("Escribe el primer numero: ");
             a = teclado.nextInt();
             System.out.println("Escribe el segundo Número:");
@@ -20,8 +25,13 @@ public class Ejercicio1{
         catch(ArithmeticException e){
             System.out.println("No se puede dividir entre 0");
         }
+        catch(FileNotFoundException e){
+            System.out.println("Archivo no encintrado");
+        }
         finally{
+            if(teclado != null){
             teclado.close();
+            }
         }
     }
 }
